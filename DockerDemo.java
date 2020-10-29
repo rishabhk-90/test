a893a2c151e0d34f3d4b185cf11fbbc130cf80cf
Rishabh Kumar <66554036+rishabhk-90@users.noreply.github.com>
2020-10-29 00:55:14 +0000

    Update DockerDemo.java

--- DockerDemo.java
+++ DockerDemo.java
@@ -2,7 +2,7 @@ public class DockerDemo{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is the output from the Hello class running using Docker");
+		System.out.println("This is the output from the DockerDemo java class running using Docker");
 
 	}
 
